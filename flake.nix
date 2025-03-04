c0a0135c0583fe4317473301df8df8196f45597f
Andreas Riegg <andreas.riegg@gmx.de>
2025-03-04 18:38:23 +0900

    remade and simplified nix flake, added direnv stuff and nix's result symlink to .gitignore

--- flake.nix
+++ flake.nix
@@ -1,54 +1,49 @@
 {
-  description = "A wrapper for ffmpeg and mp4v2 to merge, split, and manipulate audiobooks";
+  description = "Example flake for PHP development";
 
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
-    flake-utils.url = github:numtide/flake-utils;
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs =
     {
-      overlay = nixpkgs.lib.composeManyExtensions [
-        (final: prev: {
-          m4b-tool = final.callPackage ./default.nix {};
-        })
-      ];
-    } // (flake-utils.lib.eachDefaultSystem (system:
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [ self.overlay ];
+          config.allowUnfree = true;
         };
 
-        composer2NixSrc = pkgs.fetchFromGitHub {
-          owner = "svanderburg";
-          repo = "composer2nix";
-          rev = "v0.0.6";
-          sha256 = "sha256-P3acfGwHYjjZQcviPiOT7T7qzzP/drc2mibzrsrNP18=";
-        };
-
-        composer2Nix = import composer2NixSrc {
-          inherit pkgs system;
-        };
+        m4b-tool = pkgs.callPackage ./default.nix { };
       in
-      rec {
-        packages = rec {
-          m4b-tool = pkgs.m4b-tool;
+      {
+        packages = {
+          m4b-tool = m4b-tool;
           m4b-tool-libfdk = m4b-tool.override {
-            useLibfdkFfmpeg = true;
+            useLibfdk = true;
           };
-          default = m4b-tool;
         };
 
-        apps = rec {
-          m4b-tool = flake-utils.lib.mkApp { drv = self.packages.${system}.m4b-tool; };
-          default = m4b-tool;
+        devShells = {
+          default = pkgs.mkShellNoCC {
+            name = "php-devshell";
+            buildInputs = [
+              pkgs.php82Packages.composer
+            ] ++ m4b-tool.dependencies;
+
+          };
         };
 
-        devShell = pkgs.mkShell {
-          buildInputs = [
-            composer2Nix
-          ] ++ pkgs.m4b-tool.dependencies ++ pkgs.m4b-tool.devDependencies;
-        };
-      }));
+        apps = { };
+
+        formatter = pkgs.nixfmt-rfc-style;
+      }
+    );
 }
